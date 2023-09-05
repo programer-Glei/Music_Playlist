6e25033b80971d91422f219fc3b64448eddb6a66
Gleibson Pereira <gleibson.santos05@gmail.com>
2023-09-05 11:29:54 -0300

    proximo comit

--- uplaod_music.php
+++ uplaod_music.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- font awesome cdn link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/estilo.css">
     <title>Upload Music</title>
 </head>
 <body>

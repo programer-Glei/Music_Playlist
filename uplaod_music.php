45760afd6825ee7b5d18139a55d098a6ba7259f5
Gleibson Pereira <gleibson.santos05@gmail.com>
2023-09-23 20:10:20 -0300

    proximo comit

--- uplaod_music.php
+++ uplaod_music.php
@@ -20,9 +20,13 @@ include 'connect.php';
     <section class="form-container">
         <h3 class="heading">Fazer upload de música</h3>
         <form action="" method="POST" enctype="multipart/form-data">
+            <p>Nome da música <span>*</span> </p>
             <input type="text" class="box" name="name" placeholder="Digite o nome da música" required maxlength="100">
+            <p>Nome do artista</p>
             <input type="text" class="box" name="artist" placeholder="Digite o nome da artista" required maxlength="100">
+            <p>Selecione a música<span>*</span></p>
             <input type="file" name="music" id="" class="box" required accept="audio/*">
+            <p>Selecione album</p>
             <input type="file" name="album" id="" class="box" required accept="image/*">
             <input type="submit" value="Fazer upload de música" class="btn">
             <a href="index.php" class="option-btn">Voltar</a>

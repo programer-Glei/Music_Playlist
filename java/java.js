7648878f58b1059bc5f3e29846f654d82a5658ea
Gleibson Pereira <gleibson.santos05@gmail.com>
2023-12-04 10:53:40 -0300

    proximo comit

--- java/java.js
+++ java/java.js
@@ -1,3 +1,18 @@
 let playBtn = document.querySelectorAll('.paylist .box-container .box .play');
 
 let musicplayer = document.querySelector('.music-player');
+let musicAlbum = document.querySelector('.album');
+let musicName = document.querySelector('.name');
+let musicArtist = document.querySelector('.artist');
+let music = document.querySelector('.music');
+
+
+playBtn.forEach(play =>{
+    
+    play.onclick = () =>{
+        let src = play.getAttribute('data-src');
+        let box = play.parentElement.parentElement;
+        let album = box.querySelector('.album');
+        
+    }
+})
